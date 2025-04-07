231430571e57c67886e8284b2d204a045d2c441e
Plentk <149661762+Plentk@users.noreply.github.com>
2025-04-07 22:14:24 +0800

    Create all_possible_sums_of_number.py

---
+++ old_python_projects/all_possible_sums_of_number.py
@@ -0,0 +1,43 @@
+def nest(nomb):
+    return nomb
+def listlen(numb):
+    temp=0
+    lits=[]
+    templist=[]
+    for i in range(1,numb+1):
+        templist=[]
+        temp=numb
+        temp=temp+1-i
+        for k in range(1,temp+1):
+            if k==temp and temp!=1:
+                if k==temp:
+                    templist.append(numb-k+1)
+            else:
+                templist.append(1)
+        lits.append(templist)
+    return lits
+    return
+print("Welcome to the making fun of number simulator!")
+while True:
+    inp="10"#inp=input("Input Number to make fun off:\n")
+    if inp.isdigit()==False:
+        print("Data Validation Failed\nNumber to make fun of must be Integer.\nYou will be prompted to try again shortly.")
+        continue
+    inp=int(inp)
+    if inp<=0:
+        print("Data Validation Failed\nNumber to make fun of must be a positive integer.\nYou will be prompted to try again shortly.")
+        continue
+    while True:
+        check=input("Input True for all values leading up to number & False to continue program (Program is not case sensitive):\n")
+        if check.lower()=="true" or check.lower()=="false":
+            break
+        print('Data Validation Failed.\nPlease input "True" or "False"\nYou will be prompted to input again shortly.')
+    inp=int(inp)
+    break
+output=[]
+if check.lower()=="true":
+    for j in range(1,inp+1):
+        output.append(listlen(j))
+elif check.lower()=="false":
+    output=listlen(inp)
+print(output)
